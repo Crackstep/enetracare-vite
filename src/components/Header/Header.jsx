355fefebec7e562fa745a78e82bd07e6fc412ec5
Aryan <aryaney0704@gmail.com>
2024-06-20 18:36:07 +0530

    header and footer complete, need to be made responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,18 +3,25 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      <div>
-        <Link to='/' >eNetraCare</Link>
-        <p className="tagline">A visionary initiative by Innovease India Pvt. Ltd. </p>
+    <div className='flex items-center justify-around py-3 bg-teal-700 text-white'>
+      <div className='flex items-center gap-2'>
+        <div>
+          <h1 className='border-black border p-3'>Logo</h1>
+        </div>
+        <div className='flex items-start flex-col'>
+          <Link to='/' className='text-3xl font-semibold'>eNetraCare</Link>
+          <em className="tagline text-sm">A visionary initiative by Innovease India Pvt. Ltd. </em>
+        </div>
       </div>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/services'>Services</Link>
-        <Link to='/patient-resources'>Patient</Link>
-        <Link to='/testimonials'>Testimonials</Link>
-        <Link to='/contact-us'>Contact Us</Link>
+
+
+      <div className='text-md'>
+        <Link className='px-2' to='/'>Home</Link>
+        <Link className='px-2' to='/about'>About</Link>
+        <Link className='px-2' to='/services'>Services</Link>
+        <Link className='px-2' to='/patient-resources'>Patient</Link>
+        <Link className='px-2' to='/testimonials'>Testimonials</Link>
+        <Link className='px-2' to='/contact-us'>Contact Us</Link>
       </div>
     </div>
   )
