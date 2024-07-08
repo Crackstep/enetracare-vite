cda1d9dfa16ed72db21d211d37599e641c7ece6f
Aryan <aryaney0704@gmail.com>
2024-07-08 16:09:47 +0530

    about us page update

--- src/components/AboutUs/Sec2Card.jsx
+++ src/components/AboutUs/Sec2Card.jsx
@@ -15,8 +15,8 @@ function Sec2Card({
   )
 
   else return (
-    <div className=' w-[300px]  p-4 gap-4 flex items-center justify-center'>
-      <div className='flex flex-col items-center hover:scale-[1.1] hover:duration-300'>
+    <div className=' w-[300px] p-4 flex items-center justify-center'>
+      <div className='flex flex-col gap-4 items-center hover:scale-[1.1] hover:duration-300'>
         <img src={img} alt="" className='max-h-[50px]' />
         <p className='text-[#017f84] text-lg'>{text}</p>
       </div>
