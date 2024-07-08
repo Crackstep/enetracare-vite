6c327c61afe25e42a185c6a2d31444b272288535
Aryan <aryaney0704@gmail.com>
2024-07-08 16:01:16 +0530

    about us page update

--- src/components/AboutUs/Sec2Card.jsx
+++ src/components/AboutUs/Sec2Card.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 
 function Sec2Card({
-    img,
-    text,
-    isBorderAllowed
+  img,
+  text,
+  isBorderAllowed
 }) {
-    if(isBorderAllowed) return (
-        <div className='flex flex-col items-center border-r-2 w-[300px] border-[#017F84] p-4 gap-4'>
-          <img src={img} alt="" className='max-h-[50px]'/>
-          <p className='text-[#017f84] text-lg'>{text}</p>
-        </div>
-      )
+  if (isBorderAllowed) return (
+    <div className='border-r-2 w-[300px] border-[#017F84] p-4 gap-4 flex items-center justify-center'>
+      <div className='flex flex-col items-center hover:scale-[1.1] hover:duration-300'>
+        <img src={img} alt="" className='max-h-[50px]' />
+        <p className='text-[#017f84] text-lg'>{text}</p>
+      </div>
+    </div>
+  )
 
-      else return (
-        <div className='flex flex-col items-center w-[300px] p-4 gap-4'>
-          <img src={img} alt="" className='max-h-[50px]'/>
-          <p className='text-[#017f84] text-lg'>{text}</p>
-        </div>
-      )
-  
+  else return (
+    <div className=' w-[300px]  p-4 gap-4 flex items-center justify-center'>
+      <div className='flex flex-col items-center hover:scale-[1.1] hover:duration-300'>
+        <img src={img} alt="" className='max-h-[50px]' />
+        <p className='text-[#017f84] text-lg'>{text}</p>
+      </div>
+    </div>
+  )
+
 }
 
 export default Sec2Card
