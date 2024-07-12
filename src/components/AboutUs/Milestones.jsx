7ac7c1dcd2bb8fcb97129f4d92dc15cbbb79daf3
Aryan <aryaney0704@gmail.com>
2024-07-13 01:17:26 +0530

    env file update

--- src/components/AboutUs/Milestones.jsx
+++ src/components/AboutUs/Milestones.jsx
@@ -2,7 +2,46 @@ import React from 'react'
 import MSEventLeft from './MSEventLeft'
 import MSEventRight from './MSEventRight'
 
+let count = 0
+
 function Milestones() {
+
+    const createEventLeft = (text) => {
+        const element = document.createElement('div')
+        element.className = 'mt-6'
+        element.innerHTML = `<div className="flex flex-col sm:flex-row items-center">
+                <div className="flex justify-start w-full mx-auto items-center">
+                    <div className="w-1/2 pr-8">
+                        <div className="p-4 bg-white rounded shadow animation">
+                           ${text}
+                        </div>
+                    </div>
+                </div>
+                <div className="rounded-full bg-[#017F98] border-white border-4 w-8 h-8 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
+                    <i class="fa-solid fa-circle text-white"></i>
+                </div>
+            </div>`
+
+        return element
+    }
+    const createEventRight = (text) => {
+        const element = document.createElement('div')
+        element.className = 'mt-6'
+        element.innerHTML = `<div className="flex flex-col sm:flex-row items-center text-white">
+                <div className="flex justify-end w-full mx-auto items-center">
+                    <div className="w-1/2  pl-8">
+                        <div className="p-4 bg-white rounded shadow animation bg-opacity-40">
+                            ${text}
+                        </div>
+                    </div>
+                </div>
+                <div className="rounded-full bg-[#017F98] border-white border-4 w-8 h-8 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
+                    <i class="fa-solid fa-circle text-white"></i>
+                </div>
+            </div>`
+
+        return element
+    }
     return (
 
         <div className="min-h-screen bg-[#017F98] flex flex-col py-10 ">
@@ -33,36 +72,19 @@ function Milestones() {
                 </div>
 
             </div>
+            <button
+                onClick={(e) => {
+                    e.preventDefault()
+                    let element = count%2==0 ? createEventLeft('HellowTextblah blah lkfrfri') : createEventRight('HellowTextblah blah lkfrfri')
+                    count++;
+                    console.log(count)
+                    document.getElementById('events-container').appendChild(element)
+                    console.log(document.getElementById('events-container'))
+                    // element.innerText = 'Hellow'
+
+                }}
+            >Add Event</button>
         </div>
-        // <div className="min-h-screen bg-[#017F98] flex flex-col py-10 sm:py-10">
-        //     <h1 className='text-5xl text-white text-center capitalize PY-4'>OUR MILESTONES</h1>
-
-        //     <div className="py-1 sm:max-w-2xl sm:mx-auto w-full px-2 my-10 sm:px-0">
-
-        //         <div className="relative text-gray-700 antialiased text-sm font-semibold">
-
-        //             <div className="hidden sm:block w-1 bg-white absolute h-full left-1/2 transform -translate-x-1/2"></div>
-
-        //             <MSEventLeft
-        //                 text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, ipsum.' />
-        //             <MSEventRight
-        //                 text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, ipsum.' />
-        //             <MSEventLeft
-        //                 text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, ipsum.' />
-        //             <MSEventRight
-        //                 text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, ipsum.' />
-        //             <MSEventLeft
-        //                 text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, ipsum.' />
-        //             <MSEventRight
-        //                 text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, ipsum.' />
-        //             <MSEventLeft
-        //                 text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, ipsum.' />
-        //             <MSEventRight
-        //                 text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, ipsum.' />
-        //         </div>
-
-        //     </div>
-        // </div>
 
     )
 }
