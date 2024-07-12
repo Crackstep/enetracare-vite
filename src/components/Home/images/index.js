a0f8a5ef05d8ce79ba8c3838abf2d5035868e0b7
Aryan <aryaney0704@gmail.com>
2024-07-12 15:31:38 +0530

    updates on home page

--- src/components/Home/images/index.js
+++ src/components/Home/images/index.js
@@ -1,5 +1,5 @@
-import image1 from './img-1.jpeg'
-import image2 from './img-2.jpeg'
+import image1 from './img-2.jpeg'
+import image2 from './img-1.jpeg'
 import image3 from './img-3.jpeg'
 import image4 from './img-4.jpeg'
 import serviceImg from './service-img1.jpeg'
