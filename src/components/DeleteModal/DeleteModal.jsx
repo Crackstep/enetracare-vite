2c4b171b9d16d0a2a2a713dcd2b41ac5e6ebcd0b
Aryan <aryaney0704@gmail.com>
2024-07-21 14:15:32 +0530

    modal position update

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -4,7 +4,7 @@ function DeleteModal() {
     const [visibility, setVisibility] = useState('hidden')  //setVisibility('block') to display
     return (
         <div className={`${visibility}`}>
-            <div className='absolute m-auto top-[30%] left-[30%] bg-white p-12 flex flex-col gap-4 bg-opacity-65 backdrop-blur-sm shadow-lg'>
+            <div className='absolute m-auto  bg-white p-12 flex flex-col gap-4 bg-opacity-65 backdrop-blur-sm shadow-lg'>
                 <h2 className='text-3xl text-[#017f84]'>Are you sure you want to delete this?</h2>
                 <div className='flex gap-4'>
                     <button className='bg-red-700 text-white rounded-md px-3 font-semibold text-lg py-2 opacity-90 hover:opacity-100'>Delete</button>
