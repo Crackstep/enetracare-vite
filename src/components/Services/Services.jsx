2192cd44909712d962509eaa5c0fbf158e1a68f8
Aryan <aryaney0704@gmail.com>
2024-07-21 17:22:03 +0530

    changes in services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -47,8 +47,8 @@ function Services() {
         <div id="sec-3" className='flex items-center justify-around gap-8 my-2' >
           <div id="device-guide-text" className='flex gap-4'>
             <div id="sec-3-text-container">
-              <h2 className='text-[2.5vw] text-[#017F84]' id='sec-3-title'>Step title</h2>
-              <p className='text-[1.3vw] text-[#017F84] ' id='sec-3-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda tempore quisquam commodi est. Fugit, voluptatibus ab laudantium repudiandae eum culpa! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati assumenda quaerat enim autem sit pariatur accusantium eveniet tempore a adipisci.</p>
+              <h2 className='text-[2.5vw] text-[#017F84]' id='sec-3-title'>Setting up the device</h2>
+              <p className='text-[1.3vw] text-[#017F84] ' id='sec-3-text'>To set up the device, begin by switching it on. Position the device above the patient's eyes, ensuring both eyes are clearly visible through the lens. Adjust the mount to achieve a clear image of the eyes on the phone camera. Check for and eliminate any light reflections on the cornea to ensure clarity. Ensure the pupil is clearly visible. Finally, use the eNetraCare app installed on your phone to capture images of the patient's eyes. Here is a concise tutorial on setting up the eye screening device.</p>
             </div>
           </div>
           <div id="sec-3-video-container">
@@ -67,34 +67,34 @@ function Services() {
       {/* section 4 starts */}
       <div className='pb-20 bg-[#e1f9ff] px-10 pt-10' id='sec-4'>
         <h2 className='text-center text-[2.5vw] text-[#0e8ed2]' id='sec-4-title'>About eNetraCare App</h2>
-        <p className='text-[1.3vw] text-[#0E93D2] px-20 text-center mt-1 mb-10 object-fill' id='sec-4-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis harum consequuntur corporis obcaecati odio accusamus accusantium. Magnam expedita sint, sunt consequuntur, aliquid, ratione quae quod quo ad suscipit architecto eos tempora nemo. Amet esse, rem placeat laborum sapiente quia voluptatem! Here is a quick tutorial to eNetraCare App</p>
+        <p className='text-[1.3vw] text-[#0E93D2] px-20 text-center mt-1 mb-10 object-fill' id='sec-4-text'>Introducing our efficient app for managing eye patient records. It simplifies data entry and keeps patient information organized, making it ideal for healthcare professionals who need a reliable, easy-to-use solution for patient management. Streamline your workflow and ensure accurate record-keeping with ease. Enjoy seamless integration with existing systems and quick access to vital patient data.</p>
         
         <div id="app-container-wrap">
         <div id='app-container'>
         <div className='flex justify-center gap-8 my-4'>
           <AppCard
             img={APPIMAGES.app1}
-            text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, odio sed! Eveniet dolorum suscipit tempore corrupti. Possimus hic eaque est in explicabo amet eius quidem enim beatae ea? Vitae, dolores!'
+            text='Upon opening the app, you select a category based on the number of entries you need: 50, 100, 500, or 1000 entries, equivalent to the number of patients examined.'
             stepNum='1'
           />
           <AppCard
             img={APPIMAGES.app2}
-            text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, odio sed! Eveniet dolorum suscipit tempore corrupti. Possimus hic eaque est in explicabo amet eius quidem enim beatae ea? Vitae, dolores!Possimus hic eaque est in explicabo amet eius quidem enim beatae ea? Vitae, dolores!'
+            text='After selecting a category, you will be prompted to enter the corresponding license number. Once the license number is entered, you can begin entering patient details.'
             stepNum='2'
           />
           <AppCard
             img={APPIMAGES.app3}
-            text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, odio sed! Eveniet dolorum suscipit tempore corrupti. Possimus hic eaque est in explicabo amet eius quidem enim beatae ea? Vitae, dolores!Possimus hic eaque est in explicabo amet eius quidem enim beatae ea? Vitae, dolores!Possimus hic eaque est in explicabo amet eius quidem enim beatae ea? Vitae, dolores!'
+            text='Once the license number is entered, you will be directed to a form where you can fill in the detailed information of each patient. This form includes fields for the registration number, full name, age, gender, occupation,etc.'
             stepNum='3'
           />
           <AppCard
             img={APPIMAGES.app4}
-            text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, odio sed! Eveniet dolorum suscipit tempore corrupti. Possimus hic eaque est in explicabo amet eius quidem enim beatae ea? Vitae, dolores!'
+            text='After filling in the basic patient information, you will proceed to enter eye images and the eye score of the patient, which is obtained from the Snellen chart test.'
             stepNum='4'
           />
           <AppCard
             img={APPIMAGES.app5}
-            text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, odio sed! Eveniet dolorum suscipit tempore corrupti. Possimus hic eaque est in explicabo amet eius quidem enim beatae ea? Vitae, dolores!Possimus hic eaque est in explicabo amet eius quidem enim beatae ea? Vitae, dolores!'
+            text="After completing each patient entry, you will be directed to a page with three options: download the generated PDF, print it, or make a new patient entry. Once the selected category's entry limit is reached, you must select a category again."
             stepNum='5'
           />
         </div>
